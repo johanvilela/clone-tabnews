197a3437a2283c4b7c7d7464e772f9a09495164b
Johan Vilela <30961343+johanvilela@users.noreply.github.com>
2023-06-30 01:49:22 +0000

    feat: message to yas

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css"
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.message}>Você é a pessoa mais maravilhosa<br/> que eu tenho na minha vida!</div>
+      <div>Love you</div>
+      <div>🫶🏽</div>
+    </div>
+  )
+}
+
+export default Home;
