ba200343354d8d00ba60f8531439d8edfba9a018
Johan Vilela <johanvilela@hotmail.com>
2023-12-16 14:16:42 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+console.log(database);
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "Esse é o teste do pão de açúcar!" });
 }
 
